25928aa13f98e6a51c64effcb6343d4df80277ce
Tiffani (Friesendorf) Auer <frieseti@oregonstate.edu>
2019-02-06 23:35:50 -0500

    Update frieseti.buildrooms.c
    no malloc

--- frieseti.buildrooms.c
+++ frieseti.buildrooms.c
@@ -33,56 +33,48 @@ struct room {
  * Description:  *
  ********************************************************************************/
 int main(){
-	char* dirname;	
-	//create and initialize array for directory name
-	dirname = malloc(20 * sizeof(char));
+	char dirname[20];	
 	memset(dirname, '\0', 20);
+
 	//make new directory and generate files
 	makeDirectory(dirname);
-	//makeFile(dirname);
+	makeFile(dirname);
 
 	//create room map
 
 	//free allocated mem
-	free(dirname);
+	//free(dirname);
 
 	return 0;
 }
 
 /*********************************************************************************
  *Function: makeDirectory								 *
- * Description:  *
+ * Description: Takes a char pointer as an arg, and sets it to point to the new  *
+ * directory name, then creates that directory.
  ********************************************************************************/
 void makeDirectory(char* name){
-	int result, pid;
-	//char* dirname;
+	int pid;
 
-	//create and initialize array
-	//dirname = malloc(20 * sizeof(char));
-	//memset(dirname, '\0', 20);
-	
 	//get pid and append to directory name
 	pid = getpid();
 	sprintf(name, "frieseti.rooms.%d", pid);
 
 	//create directory
-	result = mkdir(name, 0755);
-	
-	//return dirname;
+	mkdir(name, 0755);
 }
 
 /********************************************************************************
  *Function: makeFile								*
  * Description: *
  *******************************************************************************/
-void makeFile(char* dirname){
-	//int i=0;
-	//char* rmnames[7];
-	//memset(rmnames, '\0', 7);
+void makeFile(char* dname){
+	int i=0;
+	char* rmnames[7];
+	memset(rmnames, '\0', 7);
 
 	//initialize array of ptrs to room names
 	//rmnames[0] = roomNames();
-	//printf("in makeFile() %s\n", rmnames[0]);
 	
 		
 }
@@ -92,5 +84,5 @@ void makeFile(char* dirname){
  * Description: *
  *******************************************************************************/
 
-void roomNames(){
+void roomNames(int i){
 }
