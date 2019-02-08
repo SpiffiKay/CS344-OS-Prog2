e783e86ec1152e8e327f20605d7979d4a0d439cd
Tiffani (Friesendorf) Auer <frieseti@oregonstate.edu>
2019-02-08 01:03:11 -0500

    Update frieseti.buildrooms.c
    Things progress

--- frieseti.buildrooms.c
+++ frieseti.buildrooms.c
@@ -21,7 +21,8 @@
 void MakeDirectory(char*);
 void MakeFile(char*);
 struct room AssignRoomName(struct room, int arr[]);
-
+void AddRandomConnection(struct room* arr[]);
+struct room GetRandomRoom(struct room* arr[]);
 
 //room struct
 struct room {
@@ -37,12 +38,14 @@ struct room {
  * Description:  *
  ********************************************************************************/
 int main(){
-	char dirname[20];	
-	int usednames[7];
-	memset(dirname, '\0', 20);
-
 	//seed rand
 	srand(time(NULL));
+
+	char dirname[20];	
+	memset(dirname, '\0', 20);
+	int* usednames = calloc(7,sizeof(int));
+	memset(usednames, '\0', 7);
+	struct room* myrooms[7];	
 
 	//create room structs
 	struct room rm0;
@@ -50,54 +53,69 @@ int main(){
 	rm0.name = calloc(9, sizeof(char));
 	memset(rm0.name, '\0', 9);
 	rm0 = AssignRoomName(rm0, usednames);
-	printf("rm0 name: %s\n", rm0.name);
+	myrooms[0] = &rm0;
+	//printf("rm0 name: %s\n", myrooms[0]->name);
 
 	struct room rm1;
 	rm1.id = 1;
 	rm1.name = calloc(9, sizeof(char));
 	memset(rm1.name, '\0', 9);
 	rm1 = AssignRoomName(rm1, usednames);
-	printf("rm1 name: %s\n", rm1.name);
+	myrooms[1] = &rm1;
+	//printf("rm1 name: %s\n", myrooms[1]->name);
 
 	struct room rm2;
 	rm2.id = 2;
 	rm2.name = calloc(9, sizeof(char));
 	memset(rm2.name, '\0', 9);
 	rm2 = AssignRoomName(rm2, usednames);
-	printf("rm2 name: %s\n", rm2.name);
+	myrooms[2] = &rm2;
+	//printf("rm2 name: %s\n", myrooms[2]->name);
 
 	struct room rm3;
 	rm3.id = 3;
 	rm3.name = calloc(9, sizeof(char));
 	memset(rm3.name, '\0', 9);
 	rm3 = AssignRoomName(rm3, usednames);
-	printf("rm3 name: %s\n", rm3.name);
+	myrooms[3] = &rm3;
+	//printf("rm3 name: %s\n", myrooms[3]->name);
 
 	struct room rm4;
 	rm4.id = 4;
 	rm4.name = calloc(9, sizeof(char));
 	memset(rm4.name, '\0', 9);
 	rm4 = AssignRoomName(rm4, usednames);
-	printf("rm4 name: %s\n", rm4.name);
+	myrooms[4] = &rm4;
+	//printf("rm4 name: %s\n", myrooms[4]->name);
 
 	struct room rm5;
 	rm5.id = 5;
 	rm5.name = calloc(9, sizeof(char));
 	memset(rm5.name, '\0', 9);
 	rm5 = AssignRoomName(rm5, usednames);
-	printf("rm5 name: %s\n", rm5.name);
+	myrooms[5] = &rm5;
+	//printf("rm5 name: %s\n", myrooms[5]->name);
 
 	struct room rm6;
 	rm6.id = 6;
 	rm6.name = calloc(9, sizeof(char));
 	memset(rm6.name, '\0', 9);
 	rm6 = AssignRoomName(rm6, usednames);
-	printf("rm6 name: %s\n", rm6.name);
+	myrooms[6] = &rm6;
+	//printf("rm6 name: %s\n", myrooms[6]->name);
 
-
-
-	//make game graph
 	
+	//make game graph
+	// Create all connections in graph
+	//while (IsGraphFull() == false)
+	//{
+		 AddRandomConnection(myrooms);
+	//}
+
+	// Returns true if all rooms have 3 to 6 outbound connections, false otherwise
+	//bool IsGraphFull()  
+	//{
+	//}
 
 	//make new directory and generate files
 	//MakeDirectory(dirname);
@@ -111,6 +129,7 @@ int main(){
 	free(rm4.name);
 	free(rm5.name);
 	free(rm6.name);
+	free(usednames);
 
 	return 0;
 }
@@ -145,43 +164,6 @@ void MakeFile(char* dname){
 	//create room files
 	for(i; i < 10; i++)
 	{
-		memset(file, '\0', 20);
-		
-		//roomnames
-		switch (i)
-		{
-			case 0:
-				rmname = "utopia";
-				break;
-			case 1:
-				rmname = "notopia";
-				break; 	
-			case 2:
-				rmname = "youtopia";
-				break; 
-			case 3:
-				rmname = "mehtopia";
-				break; 	
-			case 4:
-				rmname = "cattopia";
-				break;
-			case 5:
-				rmname = "dogtopia";
-				break; 	
-			case 6:
-				rmname = "bottopia";
-				break;
-			case 7:
-				rmname = "dietopia";
-				break;
-			case 8:
-				rmname = "mootopia";	
-				break; 	
-			case 9:
-				rmname = "vimtopia";
-				break;	
-		}
-		
 		//create file path
 		sprintf(file, "%s/%s", dname, rmname);	
 
@@ -198,16 +180,30 @@ void MakeFile(char* dname){
 
 /********************************************************************************
  *Function: AssignRoomName							*
- * Description: *
+ * Description: Takes a room struct and an array of ints keepings track of      *
+ * already assigned names. It then uses rand() to randomly assign room names to * 
+ * the struct passed. Once a new room name is chosen, the struct is set to 	*
+ * point at the name, the number representing the name is saved to the array of`*
+ * ints, then the struct is returned.						*
  *******************************************************************************/
 struct room AssignRoomName(struct room blank, int assigned[]){
 	struct room newname = blank;
-	int i = 0;
+	int r = 0, i = 0;
 	
-	i = rand() % 10;
+	//randomly assign roomname
+	r = rand() % 10;
+	for(i; i < newname.id; i++) 
+	{
+		//check if name has been used already, if so, get new #
+		 if(r == assigned[i])
+	  	{
+			r = rand() % 10;
+			i = -1;
+		}
+	}
 	
-	
-	switch (i)
+	//set struct pointer to assigned roomname
+	switch (r)
 	{
 		case 0:
 			sprintf(newname.name, "Utopia");
@@ -240,6 +236,78 @@ struct room AssignRoomName(struct room blank, int assigned[]){
 			sprintf(newname.name, "VimTopia");
 			break;	
 	}
+
+	//keep track of roomnames already used
+	assigned[newname.id] = r;
+
 	return newname;
 }
 
+/********************************************************************************
+ *Function: AddRandomConnection							*
+ * Description: * Adds a random, valid outbound connection from a Room to
+ *  another Room
+ *******************************************************************************/
+void AddRandomConnection(struct room* rooms[])  
+{
+	struct room a;  
+	struct room b;
+
+	a = GetRandomRoom(rooms);
+	printf("a: %s\n", a.name);
+//	while(true)
+//	{
+	
+		a = GetRandomRoom(rooms);
+		printf("a: %s\n", a.name);
+
+//		if (CanAddConnectionFrom(A) == true)
+//			 break;
+//	}
+
+//	do
+//	{
+//		B = GetRandomRoom();
+//	}
+//	while(CanAddConnectionFrom(B) == false || IsSameRoom(A, B) == true || ConnectionAlreadyExists(A, B) == true);
+
+//	ConnectRoom(A, B);  // TODO: Add this connection to the real variables, 
+//	ConnectRoom(B, A);  //  because this A and B will be destroyed when this function terminates
+}
+
+/********************************************************************************
+ *Function: GetRandomRoom							*
+ * Description: *Returns a random Room, does NOT validate if connection can be 
+ * added
+ *******************************************************************************/
+struct room GetRandomRoom(struct room* rooms[])
+{
+	int r = 0;
+	struct room random;
+
+	//randomly assign roomname
+	r = rand() % 7;
+	random = *rooms[r];
+	printf("random: %s\n", random.name);	
+	return random;
+}
+
+// Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise
+//bool CanAddConnectionFrom(Room x) 
+//{
+//}
+
+// Returns true if a connection from Room x to Room y already exists, false otherwise
+//bool ConnectionAlreadyExists(x, y)
+//{
+//}
+
+// Connects Rooms x and y together, does not check if this connection is valid
+//void ConnectRoom(Room x, Room y) 
+//{
+//}
+
+// Returns true if Rooms x and y are the same Room, false otherwise
+//bool IsSameRoom(Room x, Room y) 
+//{
+//}
