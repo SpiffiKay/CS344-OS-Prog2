6af3bd382cf1a1ea3213252e3af3894a18c7e304
Tiffani (Friesendorf) Auer <frieseti@oregonstate.edu>
2019-02-12 19:16:51 -0500

    Update frieseti.buildrooms.c
    structs completed

--- frieseti.buildrooms.c
+++ frieseti.buildrooms.c
@@ -23,28 +23,33 @@ void MakeFile(char*);
 struct room AssignRoomName(struct room, int arr[]);
 void AddRandomConnection(struct room* arr);
 struct room GetRandomRoom(struct room* arr);
-int CanAddConnectionFrom(struct room); 
+int CanAddConnectionFrom(int); 
 int ConnectionAlreadyExists(int, struct room);
 int IsSameRoom(int, int); 
 void ConnectRoom(struct room, struct room);
 int IsGraphFull(struct room* arr);
-
+void SetRoomsOrder(struct room* arr);
   
 //room struct
 struct room {
 	int id;
 	char* name;
-	int cnct;
 	char* rtype;
+	int cnct;
 	struct room* outcncts;
 };
 
-/*********************************************************************************
- *Function: main								 *
- * Description:  *
- ********************************************************************************/
+/********************************************************************************
+*Function: main									*
+* Description: Initializes all structs, as well as pointers to the structs. It 	*
+* also initializes an array that keeps track of what room names that have been  *
+* assigned. It then calls the appropriate functions to set up the rooms and 	*
+* graph as needed for the adventure game. It then frees all dynamically alloc	*
+* memory.									*
+********************************************************************************/
 int main(){
 	//seed rand
+	srand(time(0));
 	char dirname[20];	
 	memset(dirname, '\0', 20);
 	int* usednames = calloc(7,sizeof(int));
@@ -58,90 +63,95 @@ int main(){
 	rm0.id = 0;
 	rm0.name = calloc(9, sizeof(char));
 	memset(rm0.name, '\0', 9);
+	rm0.rtype = calloc(4, sizeof(char));
+	memset(rm0.rtype, '\0', 4);
 	rm0.cnct = 0;
 	rm0.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm0.outcncts, '\0', 6);
 	rm0 = AssignRoomName(rm0, usednames);
 	myrooms[0] = rm0;
-	//printf("rm0 name: %s\n", myrooms[0]->name);
 
 	struct room rm1;
 	rm1.id = 1;
 	rm1.name = calloc(9, sizeof(char));
 	memset(rm1.name, '\0', 9);
+	rm1.rtype = calloc(4, sizeof(char));
+	memset(rm1.rtype, '\0', 4);
 	rm1.cnct = 0;
 	rm1.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm1.outcncts, '\0', 6);
 	rm1 = AssignRoomName(rm1, usednames);
 	myrooms[1] = rm1;
-	//printf("rm1 name: %s\n", myrooms[1]->name);
 
 	struct room rm2;
 	rm2.id = 2;
 	rm2.name = calloc(9, sizeof(char));
 	memset(rm2.name, '\0', 9);
+	rm2.rtype = calloc(4, sizeof(char));
+	memset(rm2.rtype, '\0', 4);
 	rm2.cnct = 0;
 	rm2.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm2.outcncts, '\0', 6);
 	rm2 = AssignRoomName(rm2, usednames);
 	myrooms[2] = rm2;
-	//printf("rm2 name: %s\n", myrooms[2]->name);
 
 	struct room rm3;
 	rm3.id = 3;
 	rm3.name = calloc(9, sizeof(char));
 	memset(rm3.name, '\0', 9);
+	rm3.rtype = calloc(4, sizeof(char));
+	memset(rm3.rtype, '\0', 4);
 	rm3.cnct = 0;
 	rm3.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm3.outcncts, '\0', 6);
 	rm3 = AssignRoomName(rm3, usednames);
 	myrooms[3] = rm3;
-	//printf("rm3 name: %s\n", myrooms[3]->name);
 
 	struct room rm4;
 	rm4.id = 4;
 	rm4.name = calloc(9, sizeof(char));
 	memset(rm4.name, '\0', 9);
+	rm4.rtype = calloc(4, sizeof(char));
+	memset(rm4.rtype, '\0', 4);
 	rm4.cnct = 0;
 	rm4.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm4.outcncts, '\0', 6);
 	rm4 = AssignRoomName(rm4, usednames);
 	myrooms[4] = rm4;
-	//printf("rm4 name: %s\n", myrooms[4]->name);
 
 	struct room rm5;
 	rm5.id = 5;
 	rm5.name = calloc(9, sizeof(char));
 	memset(rm5.name, '\0', 9);
+	rm5.rtype = calloc(4, sizeof(char));
+	memset(rm5.rtype, '\0', 4);
 	rm5.cnct = 0;
 	rm5.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm5.outcncts, '\0', 6);
 	rm5 = AssignRoomName(rm5, usednames);
 	myrooms[5] = rm5;
-	//printf("rm5 name: %s\n", myrooms[5]->name);
 
 	struct room rm6;
 	rm6.id = 6;
 	rm6.name = calloc(9, sizeof(char));
 	memset(rm6.name, '\0', 9);
+	rm6.rtype = calloc(4, sizeof(char));
+	memset(rm6.rtype, '\0', 4);
 	rm6.cnct = 0;
 	rm6.outcncts = (struct room*) malloc(6 * sizeof(struct room));
 	memset(rm6.outcncts, '\0', 6);
 	rm6 = AssignRoomName(rm6, usednames);
 	myrooms[6] = rm6;
-	//printf("rm6 name: %s\n", myrooms[6]->name);
 
-	
 	//make game graph
+	SetRoomsOrder(myrooms);
 	while (IsGraphFull(myrooms) == 0)
 	{
 		 AddRandomConnection(myrooms);
 	}
-
-//	IsGraphFull(myrooms);
-
+	
 	//make new directory and generate files
-	//MakeDirectory(dirname);
+	MakeDirectory(dirname);
 	//MakeFile(dirname);
 
 	//free allocated mem
@@ -159,17 +169,24 @@ int main(){
 	free(rm4.outcncts);
 	free(rm5.outcncts);
 	free(rm6.outcncts);
+	free(rm0.rtype);
+	free(rm1.rtype);
+	free(rm2.rtype);
+	free(rm3.rtype);
+	free(rm4.rtype);
+	free(rm5.rtype);
+	free(rm6.rtype);
 	free(usednames);
 	free(myrooms);
 
 	return 0;
 }
 
-/*********************************************************************************
- *Function: MakeDirectory								 *
- * Description: Takes a char pointer as an arg, and sets it to point to the new  *
- * directory name, then creates that directory.
- ********************************************************************************/
+/********************************************************************************
+*Function: MakeDirectory							*
+* Description: Takes a char pointer as an arg, and sets it to point to the new  *
+* directory name, then creates that directory.					*
+*******************************************************************************/
 void MakeDirectory(char* name){
 	int pid;
 
@@ -182,10 +199,10 @@ void MakeDirectory(char* name){
 }
 
 /********************************************************************************
- *Function: MakeFile								*
- * Description: Takes a pointer to the directory name as an arg, then creates   *
- * files in said directory.
- *******************************************************************************/
+*Function: MakeFile								*
+* Description: Takes a pointer to the directory name as an arg, then creates    *
+* files in said directory.							*
+********************************************************************************/
 void MakeFile(char* dname){
 	int file_descriptor, i=0;
 	char* rmname;
@@ -210,13 +227,13 @@ void MakeFile(char* dname){
 }
 
 /********************************************************************************
- *Function: AssignRoomName							*
- * Description: Takes a room struct and an array of ints keepings track of      *
- * already assigned names. It then uses rand() to randomly assign room names to * 
- * the struct passed. Once a new room name is chosen, the struct is set to 	*
- * point at the name, the number representing the name is saved to the array of`*
- * ints, then the struct is returned.						*
- *******************************************************************************/
+*Function: AssignRoomName							*
+* Description: Takes a room struct and an array of ints keepings track of       *
+* already assigned names. It then uses rand() to randomly assign room names to  * 
+* the struct passed. Once a new room name is chosen, the struct is set to 	*
+* point at the name, the number representing the name is saved to the array of` *
+* ints, then the struct is returned.						*
+*******************************************************************************/
 struct room AssignRoomName(struct room blank, int assigned[]){
 	struct room newname = blank;
 	int r = 0, i = 0;
@@ -275,56 +292,50 @@ struct room AssignRoomName(struct room blank, int assigned[]){
 }
 
 /********************************************************************************
- *Function: AddRandomConnection							*
- * Description: * Adds a random, valid outbound connection from a Room to
- *  another Room
- *******************************************************************************/
+*Function: AddRandomConnection							*
+* Description: Takes an array of room structs as an arg, and generates two 	*
+* random rooms to connect, and tests if they are a valid connection. If so, 	*
+* they are connected both ways, and their connection counters are incremented.	*	
+*******************************************************************************/
 void AddRandomConnection(struct room* rooms){
 	struct room a;  
 	struct room b;
 
+	//generate first random room to connect
 	while(1)
 	{	
 		a = GetRandomRoom(rooms);
-		printf("\na: %s id: %d\n", a.name, a.id);
 		
-		if (CanAddConnectionFrom(a) == 1)
-		{	 
+		//check if connections are full
+		if (CanAddConnectionFrom(a.cnct) == 1)	 
 			break;
-		}
 	}
 
+	//generate second random room. Checks if connects are full, if same room
+	//as first room, or if room a and b are already connected. If not, they
+	//can be connected.
 	do
 	{
 		b = GetRandomRoom(rooms);
-		printf("b: %s id: %d\n", b.name, b.id);
 	}
-	//while(CanAddConnectionFrom(b) == 0 || IsSameRoom(a.id,b.id) == 1);
-	while(CanAddConnectionFrom(b) == 0 || IsSameRoom(a.id,b.id) == 1 || ConnectionAlreadyExists(a.id,b) == 1);
+	while(CanAddConnectionFrom(b.cnct) == 0 || IsSameRoom(a.id,b.id) == 1 || ConnectionAlreadyExists(a.id,b) == 1);
 
-//	ConnectionAlreadyExists(a.id,b);
 
-	//store room connections in permanent rooms
+	//store room connections in permanent rooms array
 	rooms[a.id].outcncts[rooms[a.id].cnct] = b;
 	rooms[b.id].outcncts[rooms[b.id].cnct] = a;
-	
-	
-	printf("rooms[a.id:%d].outcncts[%d] points to %s\n", a.id, rooms[a.id].cnct, rooms[a.id].outcncts[a.cnct].name);
-	printf("rooms[b.id:%d].outcncts[%d] points to %s\n", b.id, rooms[b.id].cnct, rooms[b.id].outcncts[b.cnct].name);
-	
 	
 	//increment connection counter for each room
 	rooms[a.id].cnct++;
 	rooms[b.id].cnct++;
-	
-	printf("#of connections: %s: %d, %s: %d\n\n", rooms[a.id].name, rooms[a.id].cnct, rooms[b.id].name, rooms[b.id].cnct);
 }
 
 /********************************************************************************
- *Function: GetRandomRoom							*
- * Description: *Returns a random Room, does NOT validate if connection can be 
- * added
- *******************************************************************************/
+*Function: GetRandomRoom							*
+* Description: Takes an array of room structures as an arguemnt, and generates  *
+* a random number that correlates with the ID# of a rooom. That room is saved   *
+* in a temporary struct, and returned to the calling function.			*	
+*******************************************************************************/
 struct room GetRandomRoom(struct room* rooms){
 	int r = 0;
 	struct room random;
@@ -332,77 +343,56 @@ struct room GetRandomRoom(struct room* rooms){
 	//randomly assign roomname
 	r = rand() % 7;
 	random = rooms[r];	
-
 	return random;
 }
 
 /********************************************************************************
- *Function: CanAddConnectionFrom						*
- * Description: Returns true if a connection can be added from Room x (< 6 
- * outbound connections), false otherwise
- *******************************************************************************/
-int CanAddConnectionFrom(struct room x){
-	printf("\nIN CanAddConnectionFrom: ");
-
-	//change bool to true if connections is <6
-	if(x.cnct < 6)
-	{
-		printf("connection less than 6\n");
+*Function: CanAddConnectionFrom							*
+* Description: Takes the # of connections as an argument. If it is less than	*
+* 6 returns ture, otherwise returns false.					*
+*******************************************************************************/
+int CanAddConnectionFrom(int cncts){
+	if(cncts < 6)
 		return 1;
-	}
-	printf("connections full!\n");
 	return 0;
 }
 
-/*********************************************************************************
- *Function: ConnectionAlreadyExists						 *
- * Description: * Returns true if a connection from Room x to Room y already
- * exists, false otherwise
- ********************************************************************************/
+/********************************************************************************
+*Function: ConnectionAlreadyExists						*
+* Description: Takes the ID of one room x and the struct of room y as args. It  *
+* then compares the ID of room x against the ID's of room y's connections. If   *
+* there's a connection, it returns true, otherwise, false.			* 
+*******************************************************************************/
 int ConnectionAlreadyExists(int xid, struct room y){
 	int i = 0;
 
-	printf("\nIN ConnectionAlreadyExists() xid: %d\n y.cnct: %d: ", xid, y.cnct);
-	
 	for(i; i < y.cnct; i++)
 	{
-		printf("y.outcncts[i].id: %d\n", y.outcncts[i].id);
 		if(xid == y.outcncts[i].id)
-		{
-			printf("THERE'S ALREADY A CONNECTION: xid: %d y.outcncts[i].id: %d\n", xid, y.outcncts[i].id);
 			return 1;
-		}
 	}
-	printf("NO CONNECTION!\n");
 	return 0;
 }
-	
-
-
 
 /********************************************************************************
- *Function: IsSameRoom								*
- * Description: Returns true if Rooms x and y are the same Room, false otherwise*
- *******************************************************************************/
+*Function: IsSameRoom								*
+* Description: Takes the IDs of rooms x and y and compares them. If they are 	*
+* the same it returns true. If not, it returns false.				*
+*******************************************************************************/
 int IsSameRoom(int xid, int yid){
-	printf("\nIN IsSameRoom(): ");
 	if(xid == yid)
-	{
-		printf("same room!\n");
 		return 1;
-	}
-	printf("not same room!\n");
 	return 0;
 }
 
 /********************************************************************************
- *Function: IsGraphFull								*
- * Description: Returns true if all rooms have 3 to 6 outbound connections, 	*
- * false otherwise
- *******************************************************************************/
+*Function: IsGraphFull								*
+* Description: Takes an array of rooms as an arg. It then cycles through the    *
+* rooms to check how many connections each room has. If all rooms have 3 or 	*
+* more connections it returns true, otherwise returns false.			*
+*******************************************************************************/
 int IsGraphFull(struct room* rooms){
-	int full = 0, i = 0;
-	int c0 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0;
+	int i = 0, c0 = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0;
 
 	//check connect count for each room
 	for(i; i<7; i++)
@@ -439,8 +429,23 @@ int IsGraphFull(struct room* rooms){
 	
 	//if all rooms have 3 or more connections, change bool to true	
 	if (c0 == 1 && c1 == 1 && c2 == 1 && c3 == 1 && c4 == 1 && c5 == 1 && c6 == 1)
-		full = 1;
+		return 1;
 	
-	printf("\nIN IsGraphFull. full: %d\n\n", full);
-	return full;
+	return 0;
+}
+
+/********************************************************************************
+*Function: SetRoomsOrder							*
+* Description: Takes an array of room structs as an arg. Then assigns all the 	*
+* rooms as mid rooms. The first two rooms are then reassigned as the first and  *
+* last rooms in the game.							*
+*******************************************************************************/
+void SetRoomsOrder(struct room* rooms){
+	int i = 0;
+	
+	for(i; i < 7; i++)
+		sprintf(rooms[i].rtype, "mid");
+
+	sprintf(rooms[0].rtype, "beg");
+	sprintf(rooms[1].rtype, "end");
 }
